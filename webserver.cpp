909752542d6739a331045093dc3848f7196648be
Rutger van Bergen <rbergen@xs4all.nl>
2025-07-20 16:46:40 +0200

    Make PDP panel fetch WebSocket port from webserver

--- webserver.cpp
+++ webserver.cpp
@@ -1,4 +1,5 @@
 #include "webserver.hpp"
+#include "json.hpp"
 
 WebServer::WebServer(unsigned short port, std::string content_dir)
     : port(port)
@@ -26,6 +27,13 @@ void WebServer::run()
     server = std::make_unique<httplib::Server>();
     server->set_mount_point("/", content_dir.c_str());
 
+     // Add config endpoint
+    server->Get("/config.json", [this](const httplib::Request&, httplib::Response& res) {
+        nlohmann::json config;
+        config["proxyPorts"] = proxy_ports; // will be a JSON object mapping names to ports
+        res.set_content(config.dump(), "application/json");
+    });
+
     log_info("HTTP server serving directory '%s' on port %u", content_dir.c_str(), port);
     server->listen("0.0.0.0", port);
     log_info("HTTP server stopped");
@@ -39,4 +47,8 @@ void WebServer::stop()
     if (server)
         server->stop();
     running = false;
+}
+
+void WebServer::add_proxy_port(const std::string& proxy_name, unsigned short port) {
+    proxy_ports[proxy_name] = port;
 }
